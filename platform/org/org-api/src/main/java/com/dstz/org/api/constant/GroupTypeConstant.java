df1c228cf9d07f3b8b4f6d808fc8330f24d6e0a5
wangyu <WANGYUXI8>
2022-03-18 11:29:48 +0800

    update GroupTypeConstant

--- platform/org/org-api/src/main/java/com/dstz/org/api/constant/GroupTypeConstant.java
+++ platform/org/org-api/src/main/java/com/dstz/org/api/constant/GroupTypeConstant.java
@@ -1,36 +1,75 @@
 package com.dstz.org.api.constant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 系统支持的组类型
  */
+
 public enum GroupTypeConstant {
-    ORG("org", "组织"),
-    POST("post", "岗位"),
-    ROLE("role", "角色");
+    ORG("org", "组织", new String[]{"bpm", "org"}),
+    POST("post", "岗位", new String[]{"bpm"}),
+    ROLE("role", "角色", new String[]{"bpm"});
+
     private String key;
     private String label;
+    private String[] tags;
 
-    GroupTypeConstant(String key, String label) {
+    private GroupTypeConstant(String key, String label) {
         this.key = key;
         this.label = label;
     }
 
+    private GroupTypeConstant(String key, String label, String[] tags) {
+        this.key = key;
+        this.label = label;
+        this.tags = tags;
+    }
+
     public String key() {
-        return key;
+        return this.key;
     }
 
     public String label() {
-        return label;
+        return this.label;
     }
- 
-    
-    public static GroupTypeConstant fromStr(String key) {
-    	
-        for (GroupTypeConstant e : GroupTypeConstant.values()) {
-            if (key.equals(e.key)) return e;
-        }
-        
-        return null;
-     }
 
+    public static GroupTypeConstant fromStr(String key) {
+        GroupTypeConstant[] var1 = values();
+        int var2 = var1.length;
+
+        for(int var3 = 0; var3 < var2; ++var3) {
+            GroupTypeConstant e = var1[var3];
+            if (key.equals(e.key)) {
+                return e;
+            }
+        }
+
+        return null;
+    }
+
+    public static GroupTypeConstant[] getValuesByTags(String tag) {
+        List<GroupTypeConstant> groupTypeConstants = new ArrayList();
+        GroupTypeConstant[] var2 = values();
+        int var3 = var2.length;
+
+        for(int var4 = 0; var4 < var3; ++var4) {
+            GroupTypeConstant e = var2[var4];
+            if (e.tags != null) {
+                String[] var6 = e.tags;
+                int var7 = var6.length;
+
+                for(int var8 = 0; var8 < var7; ++var8) {
+                    String enumTag = var6[var8];
+                    if (enumTag.equals(tag)) {
+                        groupTypeConstants.add(e);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return (GroupTypeConstant[])groupTypeConstants.toArray(new GroupTypeConstant[0]);
+    }
 }
